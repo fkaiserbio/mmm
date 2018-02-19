19c4169cc24d3580ecc72084175b6ed94ec45567
fkaiser <kaiser1@hs-mittweida.de>
2018-02-19 15:52:45 +0100

    updated test

--- mmm-core/src/test/java/bio/fkaiser/mmm/model/ItemTest.java
+++ mmm-core/src/test/java/bio/fkaiser/mmm/model/ItemTest.java
@@ -1,16 +1,13 @@
 package bio.fkaiser.mmm.model;
 
-import de.bioforscher.singa.mathematics.vectors.Vectors3D;
 import de.bioforscher.singa.structure.algorithms.superimposition.fit3d.representations.RepresentationSchemeType;
 import de.bioforscher.singa.structure.model.families.AminoAcidFamily;
 import de.bioforscher.singa.structure.model.identifiers.LeafIdentifier;
 import de.bioforscher.singa.structure.model.interfaces.AminoAcid;
-import de.bioforscher.singa.structure.model.interfaces.Atom;
 import de.bioforscher.singa.structure.model.oak.OakAminoAcid;
 import org.junit.Test;
 
 import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 
@@ -23,9 +20,7 @@ public class ItemTest {
     public void shouldGetPosition() {
         AminoAcid aminoAcid = AminoAcidFamily.GLYCINE.getPrototype();
         Item<String> item = new Item<>("A", aminoAcid);
-        assertEquals(Vectors3D.getCentroid(aminoAcid.getAllAtoms().stream()
-                                                    .map(Atom::getPosition)
-                                                    .collect(Collectors.toList())),
+        assertEquals(aminoAcid.getAtomByName("CA").get().getPosition(),
                      item.getPosition().orElseThrow(NoSuchElementException::new));
     }
 
